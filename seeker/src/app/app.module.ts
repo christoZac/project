1413a7ab7d6bfc6463136657737ef321652b4303
mail <122846945+christozachariya@users.noreply.github.com>
2023-12-17 12:48:43 +0530

    hh

--- seeker/src/app/app.module.ts
+++ seeker/src/app/app.module.ts
@@ -9,10 +9,19 @@ import { SidebarComponent } from './shared/components/sidebar/sidebar.component'
 // import {LoginComponent} from './auth/components/login/login.component';
 // import { RegistrationComponent } from './auth/components/registration/registration.component';
 import { LandingComponent } from './landing-page/components/landing/landing.component';
+import { ProfileComponent } from './my-profile/components/profile/profile.component';
+import { MessageParentComponent } from './messages/components/message-parent/message-parent.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent
+    // MessageParentComponent,
+    // FooterComponent,
+    // HeaderComponent,
+    // SidebarComponent
+    // ProfileComponent
     // HeaderComponent,
     // FooterComponent,
     // SidebarComponent,
